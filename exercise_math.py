7bd54637760f709dc1d4b8dc5a84160e73af110d
BautistaPerezF <162722097+BautistaPerezF@users.noreply.github.com>
2024-08-06 16:34:27 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,18 @@
 def math():
     a = 57
     b = 7
+    SUMA = a + b
+    print(SUMA)
+    RESTA = a - b
+    print(RESTA)
+    PRODUCTO = a * b
+    print(PRODUCTO)
+    PROMEDIO = (a + b) / 2
+    print(PROMEDIO)
+    COCIENTE_ENTERO = a // b
+    print(COCIENTE_ENTERO)
+    RESTO_DIVISION = a % b
+    print(RESTO_DIVISION)
+    VALOR_REAL_DIVISION = a / b
+    print(VALOR_REAL_DIVISION)
+math()
